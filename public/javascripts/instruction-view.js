db1f1c28695cd1fccf6ac1af503de50f01575b22
Judy Trinh <jtrinh74@gmail.com>
2013-09-07 14:52:59 -0400

    Instruction view displays step, differentiates btwn different cases for minutes

--- public/javascripts/instruction-view.js
+++ public/javascripts/instruction-view.js
@@ -14,17 +14,44 @@ InstructionView = Backbone.View.extend({
     },
 
     render: function(){
-
-        // string
         var instruction = this.options.instruction;
 
-        // parse string and for every 'time' encountered. for each, make a new timer
-
-
         var template =
-            "<div>"
-                + "<p>test</p>"
+            "<div class='instruction'>"
+                + "<p>" + instruction + "</p>"
             +"</div>";
+
+        // parse string and for every 'time' encountered. for each, make a new timer
+        var words = instruction.split(' ');
+        console.log(words);
+        for (var i = 0; i < words.length; i++) {
+
+            if (words[i].indexOf("minutes") != -1) {
+
+                if (i-3 >= 0 && words[i-2] == 'to') {
+                    // 'to' case: 5 to 10 minutes
+                    var time1 = parseInt(words[i-1]);
+                    var time2 = parseInt(words[i-3]);
+
+                    console.log("num to num");
+
+                } else if (i-1 >= 0) {
+                    var times = words[i-1].split('-');
+
+                    // single number: 5 minutes
+                    if (times.length === 1) {
+                        // create new timer
+                        console.log("single num");
+                    } else if (times.length > 1) {
+                        // hyphen case: 5-10 minutes
+
+                        // handle hyphen case
+                        console.log("num hyphen num")
+                    }
+
+                }
+            }
+        }
 
         $(this.el).append(template);
     }
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,6 @@
 $(function(){
-    newStepView = new InstructionView({instruction: "Cook chicken for 5 minutes."});
+
+    // test
+    newStepView = new InstructionView({instruction: "Cook chicken for 5 to 10 minutes."});
     
 });
