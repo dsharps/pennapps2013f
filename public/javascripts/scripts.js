b7ef62158bfef83c772743dd3c0a5e1a67708946
Eric Lee <ericslee@seas.upenn.edu>
2013-09-07 18:26:19 -0400

    Work on timers

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,7 @@
+var timerCount = 0;
+
 $(function(){
     newStepView = new InstructionView({instruction: "Cook chicken for 5 minutes."});
+    //newTimerView = new TimerView({});
     
 });
--- public/javascripts/timer-view.js
+++ public/javascripts/timer-view.js
@@ -1,42 +1,163 @@
-// Self-executing wrapper
 $(function() {
 
-    //##################################################################//
-    //####################### BackBone Models ##########################//
-    //##################################################################//
+    //var min = 2;
+    //var dsjafdsa = this.options.time[0];
+    //if time.size > 1
+    var totalMS;
+    var min;
+    var extraMS;
+    var MS;
+    var time = 0;
+    var interval = 1000; // ms
+    var canvas;
+    var ctx;
 
-  /*
-   * Create a timer object. Contains time and instructions
-   */
-  TimerObj = Backbone.Model.extend({
-    defaults: {
-      time: 0, // in seconds
-      instructions: '',
-    }
+    var TimerView = Backbone.View.extend({
 
-  });
-
-    //##################################################################//
-    //######################## BackBone Views ##########################//
-    //##################################################################//
-
-      // **ListView class**: Our main app view.
-    var ListView = Backbone.View.extend({
-    el: $('body'), // attaches `this.el` to an existing element.
-    // `initialize()`: Automatically called upon instantiation. Where you make all types of bindings, _excluding_ UI events, such as clicks, etc.
+    el: $('body'), 
     initialize: function(){
-      _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+        //totalMS = 80000;
+        totalMS = 2000;
+        min = Math.floor(totalMS / 60000);
+        extraMS = totalMS % 60000;
+        MS = 60000;
 
+      _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
        this.render(); // not all views are self-rendering. This one is.
     },
+
+    update: function() {
+        time+=interval;
+        this.clearCanvas();
+        this.drawTimer();
+    },
+
+    // Draw a ring with a specified radius, width, and fill
+    drawRing: function(x, y, outsideRadius, ringWidth, percentFilled, rotationAngle) {
+        context.beginPath();
+
+        context.arc(x, y, outsideRadius - ringWidth / 2, rotationAngle, (rotationAngle - 2 * Math.PI) + 2 * Math.PI * percentFilled, true);
+
+        context.lineWidth = ringWidth;
+        context.stroke();
+    },
+
+    clearCanvas: function () {
+        if(canvas != null) { canvas.width = canvas.width; }
+        //context.fillStyle = "#000000";
+        //context.fillRect(0, 0, canvas.width, canvas.height);
+    },
+
+
+    drawTimer: function()
+    {
+        if(totalMS <= 0) {
+            alert("TIMER IS DONE")
+        }
+
+        canvas = document.getElementById('timercanvas');
+        ctx = canvas.getContext('2d');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        window.drawCircles = function(x, y) {          
+            var buffer = document.createElement('canvas');
+            buffer.width = canvas.width;
+            buffer.height = canvas.height;
+            bctx = buffer.getContext('2d');
+            bctx.drawImage(canvas, 0, 0)
+            
+            var segmentsSec = totalMS/1000,
+                segmentsMin = min,
+                currentSegmentSec = 0,
+                currentSegmentMin = 0,
+                toRadians = function(deg) {
+                    return (Math.PI / 180) * deg;
+                },
+                getTickSec = function(num) {
+                    var tickSec = toRadians(360) / segmentsSec;
+                    return tickSec * num;
+                },
+                getTickMin = function(num) {
+                    var tickMin = toRadians(360) / segmentsMin;
+                    return tickMin * num;
+                },
+                segmentSec = function(start, end) {
+                    // Outer - seconds
+                    start = start || getTickSec(currentSegmentSec);
+                    end = end || getTickSec(currentSegmentSec + 1);
+                    ctx.lineWidth = 20;
+                    ctx.strokeStyle = 'rgba(51,153,255,0.3)';
+                    ctx.drawImage(buffer, 0, 0);
+                    ctx.beginPath();
+                    ctx.arc(x, y, 90, 0, end);
+                    ctx.stroke();
+                    ctx.closePath();
+                };
+                segmentMin = function(start, end) {
+                    // Inner - minutes
+                    ctx.lineWidth = 20;
+                    ctx.strokeStyle = 'rgba(102,255,102,0.3)';
+                    start = start || getTickSec(currentSegmentMin);
+                    end = end || getTickSec(currentSegmentMin + 1);
+                    ctx.drawImage(buffer, 0, 0);
+                    ctx.beginPath();
+                    ctx.arc(x, y, 60, 0, end);
+                    ctx.stroke();
+                    ctx.closePath();
+                };
+
+                function render() {
+                    // split for min and seconds
+                    // Seconds
+                segmentSec(getTickSec(currentSegmentSec), getTickSec(currentSegmentSec + 1));
+                currentSegmentSec += 1;
+                totalMS -= 1000;
+                if (currentSegmentSec < segmentsSec) {
+                    setTimeout(render, 1000);
+                } else {
+                    currentTickSec = 0;
+                }
+
+                // Min
+                if(time % 60000 == 0) {
+                    segmentMin(getTickMin(currentSegmentMin), getTickMin(currentSegmentMin + 1));
+                    currentSegmentMin += 1;
+                    if (currentSegmentMin < segmentsMin) {
+                        setTimeout(render, 60000);
+                    } else {
+                        currentTickMin = 0;
+                    }
+                }
+            };
+
+            render();
+        };
+
+        drawCircles(100, 100);
+        //drawRing(200, 200, 90, 80, 70, 30);
+    },
+
+    startTimer: function()
+    {
+
+    },    
     // `render()`: Function in charge of rendering the entire view in `this.el`. Needs to be manually called by the user.
     render: function(){
-      $(this.el).append("Hi");
+
+      var template = "<canvas id='timercanvas' style='z-index: 0;'> </canvas>"
+                    + "Sample cooking clip..." 
+      $(this.el).append(template);
+
+      setInterval(this.update(), interval);
+
+      var timeDisplay = min + " " + (extraMS / 1000);
+      $(this.el).append(timeDisplay);
     }
   });
 
-  // **listView instance**: Instantiate main app view.
-  var listView = new ListView();
+  // Test TimerView
+  var TimerView = new TimerView();
 });
 
 
